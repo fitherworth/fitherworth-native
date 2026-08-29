76e293be0a7dc7d9c2f93655f087fdd5dd32286f
fitherworth <support@fitherworth.com>
2026-08-29 03:30:13 +0000

    Remove native diagnostics UI

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    NativeDiagnostics.shared.startLaunch()
+    
     return true
     }
 
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) { 
-        NativeDiagnostics.shared.lifecycle("didEnterBackground")
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        NativeDiagnostics.shared.lifecycle("didBecomeActive")
+        
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
